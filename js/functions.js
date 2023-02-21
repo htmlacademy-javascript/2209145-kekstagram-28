669abd58722d3f8eb258ccfb55ef24a16e384c12
Des0l <ivanloid@gmail.com>
2023-02-21 15:17:24 +0400

    Добавляет дз по 2 главе

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,14 @@
-const isLessOrEqual = (string, length) =>
-  string.length <= length;
+//1. Функция для проверки длины строки
+
+const isLessOrEqual = (string, length) => string.length <= length;
+
+/*console.log('Функция для проверки длины строки');
+console.log(isLessOrEqual('проверяемая строка', 20)); // Результат: true - строка проходит по длине
+console.log(isLessOrEqual('проверяемая строка', 18)); // Результат: true - строка проходит по длине
+console.log(isLessOrEqual('проверяемая строка', 10)); // Результат: false — строка не проходит */
+
+
+//2. Функция для проверки, является ли строка палиндромом
 
 const isPalindrom = (string) => {
   const tempString = string
@@ -9,9 +18,17 @@ const isPalindrom = (string) => {
   for (let i = tempString.length - 1; i >= 0; i--) {
     reverseString += tempString.at(i);
   }
-  console.log(reverseString);
-  return tempString === returnString;
+  return tempString === reverseString;
 }
+
+
+/*console.log(isPalindrom('топот')); // Результат: true - строка является палиндромом
+console.log(isPalindrom('ДовОд')); // // Результат: true - это палиндром
+console.log(isPalindrom('Кекс')); // Результат: false - это не палиндром
+console.log(isPalindrom('Лёша на полке клопа нашёл ')); // Результат: true - это палиндром */
+
+
+//3. Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа
 
 const extractNumber = (string) => {
   if (typeof string === 'number') {
@@ -26,6 +43,17 @@ const extractNumber = (string) => {
   return parseInt(result, 10);
 }
 
+/*console.log(extractNumber('2023 год')); // Результат: число 2023
+console.log(extractNumber('ECMAScript 2022')); // Результат: число 2022
+console.log(extractNumber('1 кефир, 0.5 батона')); // Результат: число 105
+console.log(extractNumber('а я томат')); // Результат: NaN
+console.log(extractNumber(2023)); // Результат: число 2023
+console.log(extractNumber(-1)); // Результат: число 1
+console.log(extractNumber(1.5)); // Результат: число 15 */
+
+
+//4. Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
+
 const myPadStart = (string, minLength, pad) => {
   const actualPad = minLength - string.length;
   if (actualPad <= 0) {
@@ -33,3 +61,9 @@ const myPadStart = (string, minLength, pad) => {
   }
   return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
 }
+
+/*console.log(myPadStart('1', 2, '0')); // Результат: строка '01'
+console.log(myPadStart('1', 4, '0')); // Результат: строка '0001'
+console.log(myPadStart('q', 4, 'werty')); // Результат: строка 'werq'
+console.log(myPadStart('q', 4, 'we')); // Результат: строка 'wweq'
+console.log(myPadStart('qwerty', 4, '0')); // Результат: строка 'qwerty' */
